cc09d196c403703524b4d353fcb5e9703b04e372
pank7 <liyi84@gmail.com>
2011-12-26 22:56:02 +0900

    ruby practice
    class variables

---
+++ ruby_practice/ruby_practice_3.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby1.9
+
+class TestClass
+    attr_accessor :a, :b
+
+    def initialize(a = 0, b = 0)
+        @a = a
+        @b = b
+    end
+
+    def display
+        puts @b
+    end
+end
+
+t = TestClass.new
+t.a = 12
+puts t.a
+t.display
