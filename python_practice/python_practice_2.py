6607985f1db10342e8fe3a555546aca742b8555b
pank7 <liyi84@gmail.com>
2012-06-17 15:35:24 +0900

    start to play with python again

---
+++ python_practice/python_practice_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+import sys, os, site, time
+
+if __name__ == '__main__':
+    print(site.getusersitepackages())
+    print(site.getsitepackages())
+    
