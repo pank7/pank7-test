50fb47f339c593ef9e6124b0507cfb1756bf765b
pank7 <liyi84@gmail.com>
2012-11-04 15:42:57 +0900

    unknown changes...

--- python_practice/python_practice_2.py
+++ python_practice/python_practice_2.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
+#from __future__ import print_function
 import sys, os, site, time
 
 if __name__ == '__main__':
-    print(site.getusersitepackages())
-    print(site.getsitepackages())
+    # print(site.getusersitepackages())
+    # print(site.getsitepackages())
+    g = globals()
+    for k, v in g.items():
+        print(k, v)
+    for i in xrange(10):
+        print(i)
+    for k, v in enumerate(['a', 'b', 'c']):
+        print('%d => %s' % (k, v))
     
