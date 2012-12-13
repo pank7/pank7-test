2ff08cc938b82d77f882c60b0293452b4b374613
pank7 <liyi84@gmail.com>
2012-12-13 23:09:44 +0900

    demo & warm up

--- c_practice/test/test_list.c
+++ c_practice/test/test_list.c
@@ -1,5 +1,7 @@
 #include        <stdio.h>
 #include        <stdlib.h>
+#include        <string.h>
+#include        <errno.h>
 #include        "list.h"
 
 #define         BUFFSIZE        1024
@@ -7,18 +9,29 @@
 typedef struct
 {
   int           number;
+  char          name[BUFFSIZE];
   list_head     hook;
 } data_type;
 
 int
 main(int argc, char *argv[])
 {
-  list_head     *head = NULL;
   char          tmp[BUFFSIZE];
+  FILE          *fin = stdin;
 
-  while (fgets(tmp, BUFFSIZE, stdin)) {
-    fprintf(stdout, "%s", tmp);
+  if (argc > 1) {
+    fin = fopen(argv[1], "r");
   }
+  if (fin == NULL) {
+    perror(argv[1]);
+    exit(1);
+  }
+
+  while (fgets(tmp, BUFFSIZE, fin)) {
+    fprintf(stdout, "%4ld: %s", strlen(tmp), tmp);
+  }
+
+  fclose(fin);
 
   return 0;
 }
