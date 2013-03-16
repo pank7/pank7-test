7362687adef7140314c193ff1fed4909d7893b19
pank7 <liyi84@gmail.com>
2013-03-16 23:22:06 +0900

    useless

--- erlang/tut.erl
+++ erlang/tut.erl
@@ -1,9 +1,17 @@
 -module(tut).
--export([double/1, useless/1]).
+-export([double/1, useless/0, useless/1]).
 
 double(X) ->
     2 * X.
 
-useless(A) ->
-    {io:format("hello world~n"), A}.
+useless() ->
+    {io:format(<<"Hello, world!~n">>)}.
 
+useless(pank7) ->
+    {io:format(<<"Hello, my master!~n">>)};
+useless("pank7") ->
+    {io:format(<<"Hello, my master!~n">>)};
+useless(<<"pank7">>) ->
+    {io:format(<<"Hello, my master!~n">>)};
+useless(Name) ->
+    {io:format(<<"Hello, ~s!~n">>, [Name]), Name}.
