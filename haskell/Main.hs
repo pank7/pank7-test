d80f8e93456b8133a10c72a04b955d7eb01a4d6b
pank7 <liyi84@gmail.com>
2013-10-20 21:58:40 +0900

    some test haskell

---
+++ haskell/Main.hs
@@ -0,0 +1,15 @@
+module Main where
+
+import Text.Printf
+
+main :: IO ()
+main = do putStrLn "What's your name?"
+          your_name <- getLine
+          printf "Hello, %s (%d)!\n" your_name $ lstlen your_name
+
+lstlen :: [a] -> Int
+lstlen lst = lstlen' lst 0
+       where
+         lstlen' [] n = n
+         lstlen' (s:ss) n = lstlen' ss $ n + 1
+
