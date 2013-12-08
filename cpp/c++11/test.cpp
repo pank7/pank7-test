ecfc0dab479d427db06628aed01f494ce2babb21
pank7 <liyi84@gmail.com>
2013-12-08 20:56:05 +0900

    C++11 constexpr

--- cpp/c++11/test.cpp
+++ cpp/c++11/test.cpp
@@ -1,12 +1,19 @@
 #include        <iostream>
 #include        <array>
 
+constexpr int
+get_size(bool foo)
+{
+    return foo ? 10 : 20;
+}
+
 int
 main(int argc, char *argv[])
 {
-    std::array<int, 10>         a({1,2,3,4,5,6,7,8,9,0});
+    constexpr int               c = get_size(false);
+    std::array<int, c>          a({1,2,3,4,5,6,7,8,9,0});
 
-    for (auto i = 0; i < 10; ++i) {
+    for (auto i = 0; i < c; ++i) {
         a[i] = i * 10;
     }
 
@@ -14,7 +21,7 @@ main(int argc, char *argv[])
         e += 1;
     }
 
-    for (auto it = a.begin(); it != a.end(); ++it) {
+    for (auto it = a.cbegin(); it != a.cend(); ++it) {
         std::cout << *it << std::endl;
     }
 
