1dcb91f9b5d9b4a030eaf3b13a69892f69167ff3
pank7 <liyi84@gmail.com>
2013-12-14 17:04:00 +0900

    urandom & fstream

--- cpp/c++11/test.cpp
+++ cpp/c++11/test.cpp
@@ -1,4 +1,5 @@
 #include        <iostream>
+#include        <fstream>
 #include        <array>
 #include        <random>
 
@@ -17,6 +18,13 @@ main(int argc, char *argv[])
 {
     constexpr int               c = get_size(false);
     std::array<int, c>          a;
+    time_t                      seed = time(NULL);
+    std::ifstream               urandom("/dev/urandom", std::fstream::in | std::fstream::binary);
+
+    if (urandom.is_open()) {
+        urandom.read(reinterpret_cast<char *>(&seed), sizeof(seed));
+        std::cout << "Seed: " << seed << std::endl;
+    }
 
     std::uniform_int_distribution<int>  u(0, 1000);
     std::default_random_engine  re(time(NULL));
