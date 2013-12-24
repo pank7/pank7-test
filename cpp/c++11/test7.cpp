3cc17a609b5de92f6370c49274f3850df79835da
y-li <Yi_Li@voyagegroup.com>
2013-12-24 19:47:30 +0900

    lvalue, rvalue, reference, forward, move... confused

--- cpp/c++11/test7.cpp
+++ cpp/c++11/test7.cpp
@@ -30,9 +30,15 @@ public:
             std::cout << "[" << this << "|" << __LINE__ << "]: Destructor" << std::endl;
         }
     C &
-    operator=(C const &o)
+    operator=(C &o)
         {
-            std::cout << "[" << this << "|" << __LINE__ << "]: Operator =" << std::endl;
+            std::cout << "[" << this << "|" << __LINE__ << "]: Operator = &" << std::endl;
+            return *this;
+        }
+    C &
+    operator=(C &&o)
+        {
+            std::cout << "[" << this << "|" << __LINE__ << "]: Operator = &&" << std::endl;
             return *this;
         }
     void
@@ -54,10 +60,15 @@ main(int argc, char *argv[])
     auto        c1 = foobar1();
     auto        c2 = c1;
     auto        c3 = C(std::forward<C>(foobar1()));
+    auto        c4 = C(foobar1());
+
+    c4 = c3;
+    c4 = foobar1();
 
     c1.hoge();
     c2.hoge();
     c3.hoge();
+    c4.hoge();
 
     return 0;
 }
